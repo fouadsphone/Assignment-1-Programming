a81d8a30b565078a2901c542502215fa8756f734
Fouad Ouardi <86013594+fouadsphone@users.noreply.github.com>
2022-02-16 21:37:37 -0500

    Add files via upload

---
+++ Assignment1-/Assignment1-/Models/Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models{
+    public class Rational
+    {
+        public int Denominator { get; private set; }
+        public int Numerator { get; private set; }
+        public Rational(int numerator = 0, int denominator = 1)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return Numerator + "/" + Denominator;
+        }
+        public void IncreaseBy(Rational other)
+        {
+            Numerator = (other.Numerator * this.Denominator) + (other.Denominator * this.Numerator);
+            Denominator = other.Denominator * this.Denominator;
+        }
+        public void DecreaseBy(Rational other)
+        {
+            Numerator = (other.Denominator * this.Numerator)-(other.Numerator * this.Denominator);
+            Denominator = other.Denominator * this.Denominator;
+        }
+    }
+}
---
+++ Assignment1-/Assignment1-/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1.Models;
+
+namespace ConsoleApp8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational num1 = new Rational(5,2);
+            Rational num2 = new Rational(2,1);
+            Rational num3 = new Rational(9,5);
+            Rational num4 = new Rational(2,8);
+
+            Console.Write("The first rational is:");
+            Console.WriteLine(" " + num1);
+
+            Console.Write("The second rational is:");
+            Console.WriteLine(" " + num2);
+
+            Console.Write("The addition of these rationals is: " + num1 + " + " + num2 + " = ");
+            num1.IncreaseBy(num2);
+            Console.WriteLine(" " + num1 + "\n");
+
+            Console.Write("The third rational is:");
+            Console.WriteLine(" " + num3);
+
+            Console.Write("The fourth rational is:");
+            Console.WriteLine(" " + num4);
+
+            Console.Write("The subtraction of these rationals is: " + num3 + " - " + num4 + " = ");
+            num3.DecreaseBy(num4);
+            Console.WriteLine(" " + num3);
+
+        }
+    }
+}
